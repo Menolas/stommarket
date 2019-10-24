d696352c42267cc8c558a73a2cc21fcae290b837
elena <olenakunina@gmail.com>
2019-10-24 21:22:10 +0200

    mobile pixel perfect

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var formMobileButton = document.querySelector(".slider-paginator .btn");
+var paginatorMarkers = document.querySelectorAll(".slider-paginator__item");
 var greeting = document.querySelector(".greeting");
 var formLoginPart = document.querySelector(".slider__item--login");
 var formLoginSabmitButton = formLoginPart.querySelector("input[type='submit']");
@@ -10,9 +11,13 @@ formMobileButton.addEventListener("click", function () {
   formLoginPart.classList.add("slider__item--shown");
   greeting.classList.add("greeting--closed");
   formMobileButton.remove();
+  paginatorMarkers[1].classList.add("slider-paginator__item--active");
+  paginatorMarkers[0].classList.remove("slider-paginator__item--active");
 });
 
 formLoginSabmitButton.addEventListener("click", function () {
   formLoginPart.classList.remove("slider__item--shown");
   formSubmitPart.classList.add("slider__item--shown");
+  paginatorMarkers[2].classList.add("slider-paginator__item--active");
+  paginatorMarkers[1].classList.remove("slider-paginator__item--active");
 });
