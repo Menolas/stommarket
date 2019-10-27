7eb16b85a1c828b76bdb25a2149c2c38ca412462
elena <olenakunina@gmail.com>
2019-10-27 23:51:49 +0100

    adaptive pixel prefect

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
 'use strict';
 
 var sliderPaginator = document.querySelector(".slider-paginator");
-var formMobileButton = sliderPaginator.querySelector(".slider-paginator .btn");
 var paginatorMarkers = sliderPaginator.querySelectorAll(".slider-paginator__item");
-var greeting = document.querySelector(".greeting");
-var formLoginPart = document.querySelector(".slider__item--login");
-var formLoginSabmitButton = formLoginPart.querySelector("input[type='submit']");
-var formSubmitPart = document.querySelector(".slider__item--submit");
+var formSwitchButton = document.querySelector(".greeting .switch__btn");
+var sliderElements = document.querySelectorAll(".slider__item");
+var formLoginButton = document.querySelector(".form-login__btn");
 
-formMobileButton.addEventListener("click", function () {
-  formLoginPart.classList.add("slider__item--shown");
-  greeting.classList.add("greeting--closed");
-  formMobileButton.remove();
+formSwitchButton.addEventListener("click", function () {
+  sliderElements[0].classList.remove("slider__item--shown");
+  sliderElements[0].classList.add(".slider__item--closed");
+  sliderElements[1].classList.add("slider__item--shown");
   paginatorMarkers[1].classList.add("slider-paginator__item--active");
   paginatorMarkers[0].classList.remove("slider-paginator__item--active");
   paginatorMarkers[1].classList.remove("slider-paginator__item--strange-unknowing-blue-point");
 });
 
-console.log(sliderPaginator);
+formLoginButton.addEventListener("click", function () {
+  sliderElements[1].classList.remove("slider__item--shown");
+  sliderElements[1].classList.add("slider__item--closed");
+  sliderElements[2].classList.add("slider__item--shown");
 
-formLoginSabmitButton.addEventListener("click", function () {
-  formLoginPart.classList.remove("slider__item--shown");
-  formSubmitPart.classList.add("slider__item--shown");
   paginatorMarkers[2].classList.add("slider-paginator__item--active");
   paginatorMarkers[1].classList.remove("slider-paginator__item--active");
 });
-
-//sliderPaginator.bottom = "25px";
